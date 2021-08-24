3b57188f851f38da21bd9eb1b30326b03255adcc
4yop <1131559748@qq.com>
2021-08-24 22:08:51 +0800

    'study-go'

--- db/userfile.go
+++ db/userfile.go
@@ -6,6 +6,14 @@ import (
 	"time"
 )
 
+type UserFile struct {
+	fileSha1 string
+	fileSize int64
+	fileName string
+	uploadAt string
+	lastUpload string
+}
+
 func OnUserFileUploadFinish (username string, fileSha1 string, fileSize int64, fileName string)bool {
 	sql := "INSERT INTO `tbl_user_file`(`user_name`,`file_sha1`,`file_size`,`file_name`,`last_update`) VALUES (?,?,?,?,?)"
 	stmt,err := mysql.DbConn().Prepare(sql)
@@ -24,6 +32,13 @@ func OnUserFileUploadFinish (username string, fileSha1 string, fileSize int64, f
 }
 
 
-func QueryUserFileMeta (username string,limit int) {
 
+func QueryUserFileMeta (username string,limit int) {
+	sql := "SELECT * FROM `tbl_user_file` WHERE `user_name` = ? LIMIT ? "
+	stmt,err := mysql.DbConn().Prepare(sql)
+	if err != nil {
+		fmt.Printf(" QueryUserFileMeta Prepare err:%s\n",err)
+		return nil
+	}
+	defer stmt.Close()
 }
