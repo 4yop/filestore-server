54621a34e3bfc2330a56cc6c095def60a5349059
4yop <1131559748@qq.com>
2021-08-24 21:48:46 +0800

    'study-go'

--- db/userfile.go
+++ db/userfile.go
@@ -1,6 +1,18 @@
 package db
 
+import (
+	"filestore-server/db/mysql"
+	"fmt"
+)
 
-func OnUserFileUploadFinish (username string,file_sha1 string,file_size int64,file_name string, ) {
+func OnUserFileUploadFinish (username string,file_sha1 string,file_size int64,file_name string) {
+	sql := "INSERT INTO `tbl_user_file`(`user_name`,`file_sha1`,`file_size`,`file_name`) VALUES (?,?,?,?)"
+	stmt,err := mysql.DbConn().Prepare(sql)
+	if err != nil {
+		fmt.Printf(" OnUserFileUploadFinish Prepare err:%s\n",err)
+	}
+	defer stmt.Close()
+
+
 
 }
