60b9a26955aeeeb44af1057f33fad187ed830605
4yop <1131559748@qq.com>
2021-08-24 22:05:10 +0800

    'study-go'

--- db/userfile.go
+++ db/userfile.go
@@ -3,10 +3,11 @@ package db
 import (
 	"filestore-server/db/mysql"
 	"fmt"
+	"time"
 )
 
 func OnUserFileUploadFinish (username string, fileSha1 string, fileSize int64, fileName string)bool {
-	sql := "INSERT INTO `tbl_user_file`(`user_name`,`file_sha1`,`file_size`,`file_name`) VALUES (?,?,?,?)"
+	sql := "INSERT INTO `tbl_user_file`(`user_name`,`file_sha1`,`file_size`,`file_name`,`last_update`) VALUES (?,?,?,?,?)"
 	stmt,err := mysql.DbConn().Prepare(sql)
 	if err != nil {
 		fmt.Printf(" OnUserFileUploadFinish Prepare err:%s\n",err)
@@ -14,10 +15,15 @@ func OnUserFileUploadFinish (username string, fileSha1 string, fileSize int64, f
 	}
 	defer stmt.Close()
 
-	_,err = stmt.Exec(username, fileSha1, fileSize, fileName)
+	_,err = stmt.Exec(username, fileSha1, fileSize, fileName,time.Now())
 	if err != nil {
 		fmt.Printf(" OnUserFileUploadFinish stmt.Exec err:%s\n",err)
 		return false
 	}
 	return true
+}
+
+
+func QueryUserFileMeta (username string,limit int) {
+
 }
