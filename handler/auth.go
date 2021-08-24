112a84edd7d1a78a2d8c1c9c7d2b57fe43223bbb
lzh <1131559748@qq.com>
2021-08-24 13:57:17 +0800

    'study-go'

--- handler/auth.go
+++ handler/auth.go
@@ -1,5 +1,9 @@
 package handler
 
-func ()  {
+import "net/http"
+
+//http 请求拦截器
+func HTTPInterceptor(f func(w http.ResponseWriter,r *http.Request)) (func(w http.ResponseWriter,r *http.Request)) {
 	
+	return f
 }
