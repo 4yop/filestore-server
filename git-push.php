b5ac2222dd62bd345c1842bd4015e2156bc65d43
lzh <1131559748@qq.com>
2021-08-25 17:10:24 +0800

    'study-go'

--- git-push.php
+++ git-push.php
@@ -2,9 +2,10 @@
 
 while (true)
 {
+    $time = time();
     echo "\n---------start-----------\n";
     `git pull`;
     `git add -A && git commit -m 'study-go' && git push`;
-    echo "\n---------end-----------\n";
+    echo "\n---------end {$time}-----------\n";
     sleep(600);
 }
